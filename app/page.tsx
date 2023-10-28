d19ec901f1f669f4e82b4838025ccca497eaab57
Emmzy-Tech <emmzytech@gmail.com>
2023-10-28 13:12:05 -0400

    Started the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import NavMenu from './components/NavMenu/NavMenu'
+import LandingPage from './components/LandingPage/LandingPage'
 
 export default function Home() {
   return (
    <main>
-    <h1>Hello World</h1>
-    <Link href="/users">Users</Link>
+    <NavMenu />
+    <LandingPage />
    </main>
   )
 }
